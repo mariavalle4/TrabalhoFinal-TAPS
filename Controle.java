8f6616c3be054bbf98f886412bb51cc68ef95844
Maria Eduard Valle <164665826+mariavalle4@users.noreply.github.com>
2024-06-27 15:29:16 -0300

    Create Controle.java

---
+++ Controle.java
@@ -0,0 +1,65 @@
+public class Controle implements ActionListener{
+	
+	private JanelaPrincipal jan;
+	private Modelo m;
+	private Dao dao;
+	
+	public Controle(JanelaPrincipal jan, Modelo m) {
+		this.jan = jan;
+		this.m = m;
+		dao = new Dao();
+		registraListeneracao();
+	}
+
+	private void registraListeneracao() {
+		jan.getButtonCadastrar1().addActionListener(this);
+		jan.getButtonLimpar().addActionListener(this);
+		jan.getButtonLimpar1().addActionListener(this);
+		jan.getButtonCadastrar().addActionListener(this);
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		// TODO Auto-generated method stub
+		if(e.getActionCommand().equals("Limpar"))
+		{
+			jan.limpatelaCadastrar();
+			jan.limpatelaConsultar();
+		}
+		else if(e.getActionCommand().equals("Cadastrar"))
+		{
+			String CPF = jan.getTextFieldCPF().getText();
+			String nome = jan.getTextFieldNome().getText();
+			String endereco = jan.getTextFieldEndereco().getText();
+			
+			m.setCPF(CPF);
+			m.setNome(nome);
+			m.setEndereco(endereco);
+			
+			if(dao.cadastraPaciente(m))
+			{
+				System.out.println("Dados cadastrados com sucesso!");
+				jan.limpatelaCadastrar();
+			}
+			else
+			{
+				System.out.println("Dados não foram cadastrados!!");
+				jan.limpatelaCadastrar();
+			}
+		}
+		else if(e.getActionCommand().equals("Consultar"))
+		{
+			String CPF = jan.getTextFieldCPF1().getText();
+			m.setCPF(CPF);
+			
+			if(dao.consultaPaciente(m))
+			{
+				jan.getTextFieldNome1().setText(m.getNome());
+				jan.getTextFieldEndereco1().setText(m.getEndereco());
+		
+			}
+		}
+	}
+}
